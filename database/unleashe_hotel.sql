cf4ad57ed15d00a7c76aa468075ae069099e8241
Med Amine Nafkha <75087968+aminenafkha21@users.noreply.github.com>
2021-08-11 20:52:45 +0200

    Update unleashe_hotel.sql

--- database/unleashe_hotel.sql
+++ database/unleashe_hotel.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `unleashe_hotel`
+-- Database: `djerba_hotel`
 --
 
 -- --------------------------------------------------------
